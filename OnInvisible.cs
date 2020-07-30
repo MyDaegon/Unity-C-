32db580b785c867b2e1ff819bfc390091586e2a5
Matheus Junqueira <56365499+MyDaegon@users.noreply.github.com>
2020-07-29 23:40:33 -0300

    Add files via upload

---
+++ OnInvisible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnInvisible : MonoBehaviour
+{
+    private Rigidbody2D EnemyRB;
+    public float Velocidade;
+
+    void Start() {
+        EnemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        
+        EnemyRB.velocity = new Vector2(Velocidade, 0);
+
+    }
+    void OnBecameInvisible() {
+        
+        GameController.Dest += 1;
+        Destroy(gameObject);
+
+    }
+    
+}
