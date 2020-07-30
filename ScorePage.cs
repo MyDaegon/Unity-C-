261145c71a644537b891cd9920e52c61022a1a50
Matheus Junqueira <56365499+MyDaegon@users.noreply.github.com>
2020-07-29 23:39:58 -0300

    Add files via upload

---
+++ ScorePage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScorePage : MonoBehaviour
+{
+    public Text Pontuacao, Recorde;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Pontuacao.text = "Pontuação: " + PlayerCont.Pontuacao.ToString() + " Pontos";
+        //Tempo.text = GameController.tempo.ToString() + " Segundos";
+        if(PlayerCont.Pontuacao > PlayerPrefs.GetInt("High")){
+            PlayerPrefs.SetInt("High", PlayerCont.Pontuacao);
+        }
+        Recorde.text = "Maior Pontuação: " + PlayerPrefs.GetInt("High").ToString() + " Pontos";
+    }
+}
