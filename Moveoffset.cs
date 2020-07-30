ac5d8d9bc3919a88b431fb60586d9cc044a6d8ca
Matheus Junqueira <56365499+MyDaegon@users.noreply.github.com>
2020-07-29 23:39:33 -0300

    Add files via upload

---
+++ Moveoffset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveoffset : MonoBehaviour
+{
+    private Renderer material;
+    float p;
+    public float speedX, speedY;
+    void Start()
+    {
+        material = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p += 0.01f;
+        material.material.SetTextureOffset("_MainTex", new Vector2 (speedX * p, speedY *p));
+    }
+}
